74b2b6d98cc50e7ead62c9d18428ff6ea0794727
Víctor Díaz Fernández <diafervic@IESPIRINGALLA.AD>
2023-01-17 20:05:32 +0100

    Creadas clases archivo principal

---
+++ Main.cs
@@ -0,0 +1,25 @@
+protected class Personaje {
+    protected int vida;
+    protected int ataque;
+
+    public Personaje(
+        int vida,
+        int ataque )
+    {
+        this.vida = vida;
+        this.ataque = ataque;
+    }
+}
+
+protected class Guerrero : Personaje {
+    public Guerrero() : base (20, 10);
+}
+
+protected class Arquero : Personaje {
+    public Personaje() : base(20,5),
+}
+
+protected class Aldeano : Personaje {
+    public Aldeano(): base(20, 0);
+}
+
