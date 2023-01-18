ca43108acc4dd11a65a3d3d3e4f8ec18a5d81c29
Sonia Álvarez Brasa <alvbrason@iespiringalla.ad>
2023-01-18 19:11:51 +0100

    Probando una instancia de la clase Arquero que herede de clase abstracta

--- Main.cs
+++ Main.cs
@@ -1,28 +1,100 @@
-protected class Personaje {
+using System;
+using System.Collections.Generic;
 
-    protected int vida;
-    protected int ataque;
+// Author: Sonia Álvarez
 
-    public Personaje(
-        int vida,
-        int ataque )
+class Program
+{
+    static void Main()
     {
-        this.vida = vida;
-        this.ataque = ataque;
+        // Probando que compila y que se usan tipos enumerados
+        int fuerzaAldeana = (int)TipoPersonaje.Aldeano;
+        var tipoAldeano = TipoPersonaje.Aldeano;
+
+        Console.WriteLine( $"La fuerza de cada unidad aldeana tiene valor: {fuerzaAldeana} ");
+        Console.WriteLine( $"Cada unidad aldeana es del tipo enumerado: {tipoAldeano} ");
+
+        // Toda Unidad tiene el mismo valor entero para su salud/vida
+        int vida = 20;
+
+        // Creando una lista vacía para el equipo Azul
+        List<Personaje> EquipoAzul = new List<Personaje>();
+
+        // Probando una instancia de la clase Personaje
+        Arquero pepe = new Arquero (
+            vida, 
+            (int)TipoPersonaje.Arquero;
+        );
+
+        // Agregando un Personaje a la lista del equipo Azul
+        EquipoAzul.Add( pepe );
+
+        EquipoAzul[0].Info();
+    }
+}
+
+
+// Valores constantes de las unidades de los equipos
+// Se asigna un entero que equivale a su valor de ataque
+
+enum TipoPersonaje {
+    Aldeano = 0,
+    Arquero = 5,
+    Guerrero = 10
+}
+
+
+// Cada equipo juega con varios tipos de personajes (Unidades)
+// Cada Tipo se distingue por:
+//      su cantidad de salud/vida (En este caso igual para todas)
+//      su cantidad de fuerza de ataque
+
+public abstract class Personaje {
+    public abstract int Attack();
+    public abstract void LoseLife(int value);
+
+    public void Info() {
+        Console.WriteLine($"Este personaje tiene {Attack()} fuerza de ataque");
+    }
+}
+
+
+public class Arquero: Personaje
+{
+    private int _Vida;
+    private int _Ataque;
+
+    public Arquero (
+        int pVida,
+        int pAtaque )
+    {
+        Vida = pVida;
+        Ataque = pAtaque;
     }
 
+    public int Vida
+    {
+        get { return _Vida; }
+        set { _Vida = value; }
+    }
+
+    public int Ataque
+    {
+        get { return _Ataque; }
+        set { _Ataque = value; }
+    }
+
+    public override int Attack()
+    {
+        return Ataque;
+    }
+
+    public override void LoseLife ( int atack )
+    {
+        Vida -= atack;
+    }
 }
 
-// Esto es un comentario
-protected class Guerrero : Personaje {
-    public Guerrero() : base (20, 10) {}
-}
 
-protected class Arquero : Personaje {
-    public Personaje() : base(20, 5) {}
-}
 
-protected class Aldeano : Personaje {
-    public Aldeano(): base(20, 0) {}
-}
 
