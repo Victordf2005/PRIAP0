55b15158a9aaeabf75ab101e5409e151523b5b9a
Víctor Díaz Fernández <diafervic@IESPIRINGALLA.AD>
2023-01-19 22:47:11 +0100

    Equipos creados. Inicio ataque

--- Main.cs
+++ Main.cs
@@ -8,30 +8,50 @@ class Program
     static void Main()
     {
         // Probando que compila y que se usan tipos enumerados
-       const int fuerzaAldeana = 0;
+       const int fuerzaAldeano = 0;
        const int fuerzaGuerrero = 10;
        const int fuerzaArquero = 5;
 
-
-        
+       private bool gameOver = false;
 
         // Toda Unidad tiene el mismo valor entero para su salud/vida
         int vida = 20;
 
         // Creando una lista vacía para el equipo Azul
         List<Personaje> EquipoAzul = new List<Personaje>();
-
-        // Probando una instancia de la clase Personaje
-        Arquero pepe = new Arquero (
-            vida, 
-            fuerzaArquero 
-        );
+        List<Personaje> EquipoRojo = new List<Personaje>();
 
         // Agregando un Personaje a la lista del equipo Azul
-        EquipoAzul.Add( pepe );
 
-        EquipoAzul[0].Info();
+        EquipoAzul.Add(new Aldeano(20, fuerzaAldeano));
+        EquipoAzul.Add(new Aldeano(20, fuerzaAldeano));
+        EquipoAzul.Add(new Arquero(20, fuerzaArquero));
+        EquipoAzul.Add(new Guerrero(20, fuerzaGuerrero));
+
+
+        EquipoRojo.Add(new Aldeano(20, fuerzaAldeano));
+        EquipoRojo.Add(new Aldeano(20, fuerzaAldeano));
+        EquipoRojo.Add(new Arquero(20, fuerzaArquero));
+        EquipoRojo.Add(new Guerrero(20, fuerzaGuerrero));
+
+        while (! gameOver) {
+            LanzarAtaque(EquipoAzul, EquipoRojo);
+        }
+
+
     }
+
+
+    private LanzarAtaque(List<Personaje> equipo1, List<Personaje> equipo2) {
+
+        // escoger equipo atacante
+
+        //
+
+    }
+
+
+
 }
 
 
@@ -88,6 +108,79 @@ public class Arquero: Personaje
     {
         Vida -= atack;
     }
+}
+
+    public class Guerrero: Personaje
+{
+    private int _Vida;
+    private int _Ataque;
+
+    public Guerrero (
+        int pVida,
+        int pAtaque )
+    {
+        Vida = pVida;
+        Ataque = pAtaque;
+    }
+
+    public int Vida
+    {
+        get { return _Vida; }
+        set { _Vida = value; }
+    }
+
+    public int Ataque
+    {
+        get { return _Ataque; }
+        set { _Ataque = value; }
+    }
+
+    public override int Attack()
+    {
+        return Ataque;
+    }
+
+    public override void LoseLife ( int atack )
+    {
+        Vida -= atack;
+    }
+
+}
+    public class Aldeano: Personaje
+{
+    private int _Vida;
+    private int _Ataque;
+
+    public Aldeano (
+        int pVida,
+        int pAtaque )
+    {
+        Vida = pVida;
+        Ataque = pAtaque;
+    }
+
+    public int Vida
+    {
+        get { return _Vida; }
+        set { _Vida = value; }
+    }
+
+    public int Ataque
+    {
+        get { return _Ataque; }
+        set { _Ataque = value; }
+    }
+
+    public override int Attack()
+    {
+        return Ataque;
+    }
+
+    public override void LoseLife ( int atack )
+    {
+        Vida -= atack;
+    }
+
 }
 
 
